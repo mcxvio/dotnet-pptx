5508dc8245084cc0dfd9faddecbd1f070e4dbd8f
Marcus <mcxvio@outlook.com>
2018-03-12 14:27:27 +0000

    Initial commit mvc project.

---
+++ pptx-creator/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using pptx_creator.Models;
+using pptx_creator.Services;
+
+namespace pptx_creator.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            string filepath = @"pptx_creator.pptx";
+
+            PptxService.CreatePresentation(filepath);
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
